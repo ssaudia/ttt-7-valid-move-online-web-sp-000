a2d9e6258af82f6a0136c721d10d4398bdec1aab
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-04 02:59:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
 def valid_move?(board, index)
+  if board[index] == " "
+    true
+  elsif board[index] == "" || board[index] == nil
+    false
+  end
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
